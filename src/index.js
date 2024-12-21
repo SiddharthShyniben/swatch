07d18709dde796eabb65236e33ece6fc0a8b4ac4
Siddharth Shyniben <siddharth.muscat@gmail.com>
2024-12-21 20:57:00 +0530

    Reintroduce menu

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import keypress from "keypress";
 
 import { Screen } from "./screen.js";
+import { MenuScene } from "./menu.js";
 import { PickerScene } from "./picker.js";
 
 keypress(process.stdin);
 
 const picker = new PickerScene();
-const screen = new Screen([picker]);
+
+const screen = new Screen([
+  new MenuScene({
+    "Color Picker": picker,
+    "Palette Generator": new MenuScene(),
+    // "Gradient Generator": new MenuScene(),
+    // "Accesibility Testing": new MenuScene(),
+  }),
+  picker,
+]);
 
 process.stdin.on("keypress", function (ch, key) {
   if (key && key.ctrl && key.name === "c") {
